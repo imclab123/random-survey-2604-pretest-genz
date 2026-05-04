64418ee240d5097647884ef8fd10f1ea48ebcb0d
imclab123 <imc.lab.nccu@gmail.com>
2026-05-04 21:12:23 +0800

    Update url.js

--- url.js
+++ url.js
@@ -17,8 +17,6 @@ const urls = [
 	
 'https://zh.surveymonkey.com/r/XFR2GLF', // Overview_B_4
 	
-'https://zh.surveymonkey.com/r/7JRJYQY', // GPT_A_5
-	
 'https://zh.surveymonkey.com/r/7JXTF5V' // GPT_B_6
 	
 	];
