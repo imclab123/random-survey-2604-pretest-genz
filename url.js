22d7c62df89053ba502d4cab23662cd176464927
imclab123 <imc.lab.nccu@gmail.com>
2026-05-04 23:21:03 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,11 +9,11 @@
 
 const urls = [
 
-'https://zh.surveymonkey.com/r/X3WNFGR', // Google_B_2
+
 
 'https://zh.surveymonkey.com/r/XFR2GLF', // Overview_B_4
 
-'https://zh.surveymonkey.com/r/7JXTF5V' // GPT_B_6
+https://zh.surveymonkey.com/r/7JRJYQY, // GPT_A_5
 
 
 	
